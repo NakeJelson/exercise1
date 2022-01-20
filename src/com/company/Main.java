aba503501002b2d0d36fd64cb9128b7f9db2f00b
NakeJelson <nakejelson78@gmail.com>
2022-01-20 14:14:07 -0500

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        String city = "Columbus";
+        int zipCode = 43215;
+        double temperatures[] ={32, 25, 27, 40, 45};
+        double avgTemp = (temperatures[0] + temperatures[1] + temperatures[2] + temperatures[3] + temperatures[4])/5;
+
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+        System.out.println("Average  High Temperature: " + avgTemp);
     }
 }
